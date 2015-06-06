0e4f1e40f08e8bf8dc94850792c44e2b30c9d5b3
Jennifer Scroggins <jennifer@ravenwilde.com>
2015-06-05 23:17:15 -0400

    first iteration of word-count submitted

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,9 +1,22 @@
 var words = function words(input) {
-
-	//break string apart on spaces
-	//put each substring into an array
-	//
-
+	var sanitized = input.replace(/toString/, toString);
+	var inputArray = sanitized.replace(/\s+|\\./g," ").split( " " );
+	var outputObj = {};
+	
+	// for each array item
+	for (var i = 0; i < inputArray.length; ++i) {
+		// check if it matches a key
+		if (inputArray[i] in outputObj) {
+			// if yes, +1 to that value
+			var tmp = (inputArray[i]);
+			outputObj[tmp] += 1;	
+		} else {
+		// if no, add a new key with value = 1
+			var tmp = (inputArray[i]);
+			outputObj[tmp] = 1;
+		}
+	}
+	return outputObj;
 };
 
 
